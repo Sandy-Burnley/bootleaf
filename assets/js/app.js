35d1848b93e390bad597f1d7d01ba79dd675f685
Sandy-Burnley <51831763+Sandy-Burnley@users.noreply.github.com>
2020-01-24 14:51:53 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -124,7 +124,7 @@ var cartoLight = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox/streets-v11'
-	}).addTo(mymap);
+	}).addTo(map);
 
 /* Overlay Layers */
 var highlight = L.geoJson(null);
